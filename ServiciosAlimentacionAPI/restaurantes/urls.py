525ba49814eb52dbbb5e60f6afdfaa0db552f594
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-08-28 13:38:58 -0500

    order urls restaurantes

--- ServiciosAlimentacionAPI/restaurantes/urls.py
+++ ServiciosAlimentacionAPI/restaurantes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RetrieveUpdateDestroyTarifa, ListCreateTarifa, ListTarifaByRestaurante
 
 urlpatterns = [
-    path(r'<int:idtarifav>/<int:codarticulo>', RetrieveUpdateDestroyTarifa.as_view()),
     path(r'', ListCreateTarifa.as_view()),
+    path(r'<int:idtarifav>/<int:codarticulo>', RetrieveUpdateDestroyTarifa.as_view()),
     path(r'<int:idtarifav>/', ListTarifaByRestaurante.as_view()),
 ]
