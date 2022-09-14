264db5212d0edca3430dd43882e54109261534fc
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-09-13 19:32:46 -0500

    change tarifav urls

--- ServiciosAlimentacionAPI/restaurantes/urls.py
+++ ServiciosAlimentacionAPI/restaurantes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RetrieveUpdateDestroyTarifa, ListCreateTarifa, ListTarifaByRestaurante
 
 urlpatterns = [
-    path(r'', ListCreateTarifa.as_view()),
-    path(r'<int:idtarifav>/<int:codarticulo>', RetrieveUpdateDestroyTarifa.as_view()),
-    path(r'<int:idtarifav>/', ListTarifaByRestaurante.as_view()),
+    path('', ListCreateTarifa.as_view()),
+    path('<int:idtarifav>/<int:codarticulo>', RetrieveUpdateDestroyTarifa.as_view()),
+    path('tarifav/<int:idtarifav>/', ListTarifaByRestaurante.as_view()),
 ]
