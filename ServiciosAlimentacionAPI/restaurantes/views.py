adc8eb9f7b4242584356ade46827c8d98d186be8
jrizo0 <jr.rizo.o.jr@gmail.com>
2022-09-15 19:51:06 -0500

    change rest views

--- ServiciosAlimentacionAPI/restaurantes/views.py
+++ ServiciosAlimentacionAPI/restaurantes/views.py
@@ -8,48 +8,46 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 
-"""
-Generic Views
-"""
 class RestauranteViewSet(ModelViewSet):
     serializer_class = TarifavSerializer
     queryset = Tarifav.objects.all()
     lookup_field = "idtarifav"
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
 
     def dispatch(self, *args, **kwargs):
         return super(RestauranteViewSet, self).dispatch(*args, **kwargs)
 
-#NOTE: ModelViewSet no permite dos pk
+
+# NOTE: ModelViewSet no permite dos pk
 class MultipleFieldLookupMixin:
     def get_object(self):
-        queryset = self.get_queryset()             # Get the base queryset
+        queryset = self.get_queryset()  # Get the base queryset
         queryset = self.filter_queryset(queryset)  # Apply any filter backends
         filter = {}
         for field in self.lookup_fields:
-            if self.kwargs[field]: # Ignore empty fields.
+            if self.kwargs[field]:  # Ignore empty fields.
                 filter[field] = self.kwargs[field]
         obj = get_object_or_404(queryset, **filter)  # Lookup the object
         self.check_object_permissions(self.request, obj)
         return obj
 
-class RetrieveUpdateDestroyTarifa(MultipleFieldLookupMixin, generics.RetrieveUpdateDestroyAPIView):
+
+class RetrieveUpdateDestroyTarifa(
+    MultipleFieldLookupMixin, generics.RetrieveUpdateDestroyAPIView
+):
     serializer_class = TarifaSerializer
     queryset = Tarifa.objects.all()
-    lookup_fields = ['idtarifav', 'codarticulo']
+    lookup_fields = ["idtarifav", "codarticulo"]
+
 
 class ListCreateTarifa(MultipleFieldLookupMixin, generics.ListCreateAPIView):
     serializer_class = TarifaSerializer
     queryset = Tarifa.objects.all()
-    lookup_fields = ['idtarifav', 'codarticulo']
+    lookup_fields = ["idtarifav", "codarticulo"]
 
 
-"""
-Custom Views
-"""
 class ListTarifaByRestaurante(APIView):
     def get(self, request, idtarifav):
         data = Tarifa.objects.filter(idtarifav=idtarifav)
         serializer = TarifaSerializer(data, many=True)
         return Response(serializer.data)
-
